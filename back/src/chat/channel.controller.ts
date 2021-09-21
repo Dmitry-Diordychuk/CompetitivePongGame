bfda8a4f72840203c116b438fbe61098f81b1f26
kdustin <kdustin@student.21-school.ru>
2021-09-21 22:11:14 +0300

    add sanctions from admins

--- back/src/chat/channel.controller.ts
+++ back/src/chat/channel.controller.ts
@@ -1,10 +1,21 @@
-import {Body, Controller, Get, Param, ParseIntPipe, Put, UseGuards, UsePipes} from '@nestjs/common';
+import {
+    Body,
+    Controller,
+    Get,
+    Param,
+    ParseIntPipe,
+    Put,
+    UseGuards,
+    UsePipes,
+    ValidationPipe
+} from '@nestjs/common';
 import {ChatService} from "@app/chat/chat.service";
 import {AuthGuard} from "@app/shared/guards/auth.guard";
 import {User} from "@app/user/decorators/user.decorator";
 import {ChannelsResponseInterface} from "@app/chat/types/channelsResponse.interface";
-import {MakeAdminDto} from "@app/chat/dto/makeAdminDto";
+import {MakeAdminDto} from "@app/chat/dto/makeAdmin.dto";
 import {ChannelResponseInterface} from "@app/chat/types/channelResponse.interface";
+import {SanctionDto} from "@app/chat/dto/sanction.dto";
 
 @Controller("api/channel")
 export class ChannelController {
@@ -28,13 +39,25 @@ export class ChannelController {
         return await this.chatService.getChannel(channel_id);
     }
 
+    @UseGuards(AuthGuard)
+    @UsePipes(new ValidationPipe())
     @Put('admin')
     async makeUserChannelAdmin(
-        @Param('id') userId: number,
         @User('id') currentUserId: number,
         @Body() makeAdminDto: MakeAdminDto
     ): Promise<ChannelResponseInterface> {
         const channel = await this.chatService.makeAdmin(currentUserId, makeAdminDto.userId, makeAdminDto.channelId);
+        return {channel};
+    }
+
+    @UseGuards(AuthGuard)
+    @UsePipes(new ValidationPipe())
+    @Put('apply-sanction')
+    async applySanctionOnUser(
+        @User('id') currentUserId: number,
+        @Body() sanctionDto: SanctionDto
+    ): Promise<ChannelResponseInterface> {
+        const channel = await this.chatService.applySanctionOnUser(currentUserId, sanctionDto);
         return {channel};
     }
 }
