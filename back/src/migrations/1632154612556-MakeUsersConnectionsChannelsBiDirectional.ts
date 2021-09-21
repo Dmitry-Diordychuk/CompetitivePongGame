dcd305493a6480e59b8fc9083584a19aea1989d2
kdustin <kdustin@student.21-school.ru>
2021-09-21 22:09:00 +0300

    remove blacklist channel migration

--- back/src/migrations/1632154612556-MakeUsersConnectionsChannelsBiDirectional.ts
+++ back/src/migrations/1632154612556-MakeUsersConnectionsChannelsBiDirectional.ts
@@ -4,19 +4,19 @@ export class MakeUsersConnectionsChannelsBiDirectional1632154612556 implements M
     name = 'MakeUsersConnectionsChannelsBiDirectional1632154612556'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`CREATE TABLE "users_blacklist_channels" ("usersId" integer NOT NULL, "channelsId" integer NOT NULL, CONSTRAINT "PK_f4172e3938e33479cd6b19a6530" PRIMARY KEY ("usersId", "channelsId"))`);
-        await queryRunner.query(`CREATE INDEX "IDX_7d060ac9363d8ba897edefea66" ON "users_blacklist_channels" ("usersId") `);
-        await queryRunner.query(`CREATE INDEX "IDX_04a1fbd70d389e1fbefacde6b6" ON "users_blacklist_channels" ("channelsId") `);
-        await queryRunner.query(`ALTER TABLE "users_blacklist_channels" ADD CONSTRAINT "FK_7d060ac9363d8ba897edefea666" FOREIGN KEY ("usersId") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE`);
-        await queryRunner.query(`ALTER TABLE "users_blacklist_channels" ADD CONSTRAINT "FK_04a1fbd70d389e1fbefacde6b6a" FOREIGN KEY ("channelsId") REFERENCES "channels"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
+        // await queryRunner.query(`CREATE TABLE "users_blacklist_channels" ("usersId" integer NOT NULL, "channelsId" integer NOT NULL, CONSTRAINT "PK_f4172e3938e33479cd6b19a6530" PRIMARY KEY ("usersId", "channelsId"))`);
+        // await queryRunner.query(`CREATE INDEX "IDX_7d060ac9363d8ba897edefea66" ON "users_blacklist_channels" ("usersId") `);
+        // await queryRunner.query(`CREATE INDEX "IDX_04a1fbd70d389e1fbefacde6b6" ON "users_blacklist_channels" ("channelsId") `);
+        // await queryRunner.query(`ALTER TABLE "users_blacklist_channels" ADD CONSTRAINT "FK_7d060ac9363d8ba897edefea666" FOREIGN KEY ("usersId") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE`);
+        // await queryRunner.query(`ALTER TABLE "users_blacklist_channels" ADD CONSTRAINT "FK_04a1fbd70d389e1fbefacde6b6a" FOREIGN KEY ("channelsId") REFERENCES "channels"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "users_blacklist_channels" DROP CONSTRAINT "FK_04a1fbd70d389e1fbefacde6b6a"`);
-        await queryRunner.query(`ALTER TABLE "users_blacklist_channels" DROP CONSTRAINT "FK_7d060ac9363d8ba897edefea666"`);
-        await queryRunner.query(`DROP INDEX "IDX_04a1fbd70d389e1fbefacde6b6"`);
-        await queryRunner.query(`DROP INDEX "IDX_7d060ac9363d8ba897edefea66"`);
-        await queryRunner.query(`DROP TABLE "users_blacklist_channels"`);
+        // await queryRunner.query(`ALTER TABLE "users_blacklist_channels" DROP CONSTRAINT "FK_04a1fbd70d389e1fbefacde6b6a"`);
+        // await queryRunner.query(`ALTER TABLE "users_blacklist_channels" DROP CONSTRAINT "FK_7d060ac9363d8ba897edefea666"`);
+        // await queryRunner.query(`DROP INDEX "IDX_04a1fbd70d389e1fbefacde6b6"`);
+        // await queryRunner.query(`DROP INDEX "IDX_7d060ac9363d8ba897edefea66"`);
+        // await queryRunner.query(`DROP TABLE "users_blacklist_channels"`);
     }
 
 }
