afccedc305524a4c437cf098e08f66bdfb4f500d
kdustin <kdustin@student.21-school.ru>
2021-11-25 18:20:14 +0300

    add/delete comment

--- back/src/match/match.controller.ts
+++ back/src/match/match.controller.ts
@@ -1,5 +1,36 @@
-import {Controller} from "@nestjs/common";
+import {Body, Controller, Delete, Param, Put, UseGuards, UsePipes, ValidationPipe} from "@nestjs/common";
+import {AuthGuard} from "@app/shared/guards/auth.guard";
+import {User} from "@app/user/decorators/user.decorator";
+import {MatchService} from "@app/match/match.service";
+import {AddCommentDto} from "@app/match/dto/addComment.dto";
+import {MatchEntity} from "@app/match/match.entity";
 
 @Controller("api/match")
 export class MatchController {
+    constructor(
+        private readonly matchService: MatchService,
+    ) {
+    }
+
+    @UsePipes(new ValidationPipe)
+    @UseGuards(AuthGuard)
+    @Put('/:id/comment')
+    async addComment(
+        @Param('id') matchId: number,
+        @User('id') userId: number,
+        @Body('message') addCommentDto: AddCommentDto,
+    ): Promise<MatchEntity> {
+        return await this.matchService.addComment(matchId, userId, addCommentDto.message);
+    }
+
+    @UsePipes(new ValidationPipe)
+    @UseGuards(AuthGuard)
+    @Delete('/:id/comment/:comment_id')
+    async deleteComment(
+        @User() userId: number,
+        @Param('id') matchId: number,
+        @Param('comment_id') commentId: number,
+    ): Promise<MatchEntity> {
+        return await this.matchService.deleteComment(matchId, userId, commentId);
+    }
 }
--- back/src/match/match.service.ts
+++ back/src/match/match.service.ts
@@ -1,20 +1,64 @@
-import {Injectable} from "@nestjs/common";
+import {HttpException, HttpStatus, Injectable} from "@nestjs/common";
 import {MatchEntity} from "@app/match/match.entity";
 import {ProfileEntity} from "@app/profile/profile.entity";
 import {InjectRepository} from "@nestjs/typeorm";
 import {Repository} from "typeorm";
+import {CommentEntity} from "@app/match/comment.entity";
+import {UserService} from "@app/user/user.service";
 
 @Injectable()
 export class MatchService {
     constructor(
+        private readonly userService: UserService,
         @InjectRepository(MatchEntity) private readonly matchRepository: Repository<MatchEntity>,
+        @InjectRepository(CommentEntity) private readonly commentRepository: Repository<CommentEntity>,
     ) {}
+
+    async findMatch(matchId: number) {
+        const match = await this.matchRepository.findOne(matchId);
+        if (!match) {
+            throw new HttpException("There is no such match", HttpStatus.BAD_REQUEST);
+        }
+        return match;
+    }
 
     async createMatch(type: 'duel' | 'ladder', winner: ProfileEntity, loser: ProfileEntity): Promise<MatchEntity> {
         let match: MatchEntity = new MatchEntity();
         match.type = type;
         match.winner = winner;
         match.loser = loser;
+        return await this.matchRepository.save(match);
+    }
+
+    async addComment(matchId: number, userId: number, message: string) {
+        const match = await this.findMatch(matchId);
+        let comment: CommentEntity = new CommentEntity();
+        comment.message = message;
+        comment.author = await this.userService.getUserById(userId);
+        comment = await this.commentRepository.save(comment);
+        if (match.comments) {
+            match.comments = [comment, ...match.comments];
+        } else {
+            match.comments = [comment];
+        }
+        return match;
+    }
+
+    async deleteComment(matchId: number, userId: number, commentId: number) {
+        const match = await this.findMatch(matchId);
+        if (match.comments) {
+            const comment = await match.comments.find(c => c.id === commentId);
+            if (!comment) {
+                throw new HttpException("There is no such match", HttpStatus.BAD_REQUEST);
+            }
+            if (comment.author.id === userId) {
+                match.comments = match.comments.filter(c => c.id !== commentId);
+            } else {
+                throw new HttpException("You're not allowed to delete this comment", HttpStatus.BAD_REQUEST);
+            }
+        } else {
+            throw new HttpException("There is no such comment", HttpStatus.BAD_REQUEST);
+        }
         return await this.matchRepository.save(match);
     }
 }
