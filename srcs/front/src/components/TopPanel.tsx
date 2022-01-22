d7bd196818e5855b9e8ff5ed8f30fdfd6fd613c9
Anton Chaparin <chaparin.anton@gmail.com>
2022-01-22 21:57:29 +0300

    Put the top panel links back

--- srcs/front/src/components/TopPanel.tsx
+++ srcs/front/src/components/TopPanel.tsx
@@ -35,10 +35,8 @@ export default function TopPanel() {
                         }
                         <Nav.Link as="span"><Link style={{ textDecoration: 'none' }} to="/profile">Profile</Link></Nav.Link>
                         <Nav.Link as="span"><Link style={{ textDecoration: 'none' }} to="/channels">Channels</Link></Nav.Link>
-{/*
                         <Nav.Link as="span"><Link style={{ textDecoration: 'none' }} to="/contacts">Contacts</Link></Nav.Link>
                         <Nav.Link as="span"><Link style={{ textDecoration: 'none' }} to="/settings">Settigs</Link></Nav.Link>
-*/}
                         <Nav.Link as="span">{location.pathname !== '/game' ? <Matchmacking/> : <></>}</Nav.Link>
                         <Nav.Link as="span">{location.pathname !== '/game' ? <Duel /> : <></>}</Nav.Link>
                     </Nav>
