480e28583f416c527ac124277f1ec8c6a7ac0fbc
kdustin <kdustin@student.21-school.ru>
2022-01-23 11:53:16 +0300

    add material to dependencies, add docker-compose up –build to script

--- script.sh
+++ script.sh
@@ -4,5 +4,6 @@ docker rm $(docker ps -a -q)
 docker rmi $(docker images -q)
 docker system prune -a
 docker volume prune
-docker run --rm docker/compose
-docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v "${PWD}:${PWD}" -w="${PWD}/ft_trans" docker/compose up
+#docker run --rm docker/compose
+#docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v "${PWD}:${PWD}" -w="${PWD}/ft_trans" docker/compose up
+docker-compose up –build
