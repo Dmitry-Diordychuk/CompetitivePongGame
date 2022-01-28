24bda07b11c3309b3d8ba42a4f9e1c368a7021f0
kdustin <kdustin@student.21-school.ru>
2022-01-28 16:55:11 +0300

    Login style

--- srcs/front/src/components/Login.tsx
+++ srcs/front/src/components/Login.tsx
@@ -1,11 +1,15 @@
 import React, {useCallback, useEffect, useState} from 'react';
 import { useAuth } from "../auth/auth.context";
-import '../styles/Login.css';
 import OauthPopup from 'react-oauth-popup';
-import logo from '../static/42_logo.svg';
+import LogoIcon from '../static/42_logo.svg';
 import {useNavigate, useLocation} from "react-router-dom";
-import {Alert} from "react-bootstrap";
 import {LOGIN_REDIRECT, PORT_REDIRECT} from "../config";
+import {Alert, Box, Button, Icon, Stack, Typography} from "@mui/material";
+import BackgroundImage from '../pong.jpg';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 
 function Login()
@@ -47,14 +51,23 @@ function Login()
 		}
 	}, [navigate]);
 
-	function onCode(code: string, params: any) {
+	function onCode(code: string) {
 		auth.signin(code, handleSuccessLogin, handleLoginFailed);
 	}
 
 	return (
-		<>
-			<Alert variant={'danger'} show={show} >{message}</Alert>
-			<div className='login_input'>
+		<Box sx={{
+			background: `url(${BackgroundImage})`,
+			backgroundSize: 'cover',
+			backgroundPosition: 'top',
+			minHeight: '100%',
+			height: '100vh',
+			position: 'relative'
+		}}>
+			<Alert severity={'warning'} hidden={!show} >{message}</Alert>
+			<Box sx={{
+				textAlign: 'center',
+			}}>
 				<OauthPopup
 					url={"https://api.intra.42.fr/oauth/authorize" +
 						"?client_id=28b3bd90a12b1869322ee8df91393d90bb716604da919d12f9296cb545b832de" +
@@ -66,14 +79,42 @@ function Login()
 					width={750}
 					height={750}
 				>
-					<img alt={'42 school logo'} src={logo} className='logo-42' />
+					<Button sx={{
+						color: "#FFFFFF",
+						width: 403,
+						height: 83,
+						p: 5,
+						border: '2px solid #E2E2E2',
+						boxSizing: 'border-box',
+						borderRadius: '8px',
+						background: 'rgba(0, 0, 0, 0.35)',
+						position: 'absolute',
+						top: '40%',
+						left: '37%',
+					}} variant="outlined" size="large">
+						<Stack direction="row" spacing={2}>
+							<Typography variant="h6" component="p" gutterBottom>LOG IN VIA</Typography >
+							<Icon sx={{
+								textAlign: 'center',
+								lineHeight: '-5px',
+							}} fontSize='large'>
+								<img alt={"42Icon"} style={{
+									display: 'flex',
+									height: 'inherit',
+									width: 'inherit'
+								}} src={LogoIcon}/>
+							</Icon>
+						</Stack>
+					</Button>
 				</OauthPopup>
-			</div>
+			</Box>
 			<button onClick={() => auth.goBackdoor('A_user', ()=>{navigate("/", {replace: true})}, ()=>{})}>A_user</button>
 			<button onClick={() => auth.goBackdoor('B_user', ()=>{navigate("/", {replace: true})}, ()=>{})}>B_user</button>
 			<button onClick={() => auth.goBackdoor('C_user', ()=>{navigate("/", {replace: true})}, ()=>{})}>C_user</button>
-		</>
+		</Box>
 	)
 }
+
+// alt={'42 school logo'} src={logo} className='logo-42' />
 
 export default Login;
